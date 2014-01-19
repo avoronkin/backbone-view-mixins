e3744c978d166168135b6c2ebde0f1423840a347
Alexandr Voronkin <admin@elasticweb.ru>
2014-01-20 01:10:53 +0400

    add advice.js

--- tests/specs/afterRender.spec.js
+++ tests/specs/afterRender.spec.js
@@ -1,5 +1,24 @@
+var advice = require('Backbone.Advice');
+var afterRender = require('../../src/afterRender');
 
-describe('afterRender tests', function(){
+describe('afterRender mixin tests', function(){
+    var a;
+    beforeEach(function(){
+        var A = function(){};
+        A.prototype = {
+            render: function(){
+                console.log('render'); 
+            },
+            afterRender: function(){
+                console.log('afterRender'); 
+            }
+        };
+        advice.addMixin(A);
+        A.mixin(afterRender);
+        a = new A();
+    });
+
     it('should work', function(){
+        a.render();
     });
 });
